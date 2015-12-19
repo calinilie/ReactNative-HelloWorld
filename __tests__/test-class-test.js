e684f62566b4354921d7455d4475cfa18cb5f4d6
calinilie <calinilielaur@gmail.com>
2015-12-19 12:38:39 +0100

    initial commit

---
+++ __tests__/test-class-test.js
@@ -0,0 +1,16 @@
+jest.dontMock('../test-class');
+
+describe('test test', function() {
+
+	var underTest = require('../test-class');
+
+	it('should not blow up!', function() {
+		expect(underTest()).not.toBeUndefined();
+	});
+
+	it('should return the correct result', function() {
+		expect(underTest('Calin')).toBe('Hello world Calin!');
+	})
+
+
+})
